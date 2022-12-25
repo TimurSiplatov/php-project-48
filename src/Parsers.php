450d2714847c5f1670c8a7d911e0187c0dd3b00d
TimurSiplatov <jjj_jjj_18@mail.ru>
2022-12-25 18:04:20 +0300

    fix linter

--- src/Parsers.php
+++ src/Parsers.php
@@ -5,7 +5,7 @@ namespace Differ\Parsers;
 use Symfony\Component\Yaml\Yaml;
 
 function parseFile($pathToFile)
-{   
+{
     $fileFormat = pathinfo($pathToFile, PATHINFO_EXTENSION);
     if ($fileFormat === "json") {
         return json_decode(file_get_contents($pathToFile), FILE_USE_INCLUDE_PATH);
