9449d6e76158afff26aee9a41cd619712419711c
TimurSiplatov <jjj_jjj_18@mail.ru>
2023-01-01 16:06:52 +0300

    fix lint

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 install: # установка зависимостей
 	composer install
 lint: # линтер
-	composer exec --verbose phpcs -- --standard=PSR12 src bin
+	composer exec --verbose phpcs -- --standard=PSR12 src bin tests/test.php
 test: # тесты
 	composer exec --verbose phpunit tests/test.php
 coverage: # покрытие
--- tests/test.php
+++ tests/test.php
@@ -8,37 +8,37 @@ use function Differ\Differ\genDiff;
 
 class Test extends TestCase
 {
-  private $firstJsonFile;
-  private $secondJsonFile;
-  private $firstYmlFile;
-  private $secondYmlFile;
+    private $firstJsonFile;
+    private $secondJsonFile;
+    private $firstYmlFile;
+    private $secondYmlFile;
 
-  public function setUp(): void
-  {
-    $this->firstJsonFile = __DIR__ . "/fixtures/file1.json";
-    $this->secondJsonFile = __DIR__ . "/fixtures/file2.json";
-    $this->firstYmlFile = __DIR__ . "/fixtures/file1.yml";
-    $this->secondYmlFile = __DIR__ . "/fixtures/file2.yml";
-  }
+    public function setUp(): void
+    {
+        $this->firstJsonFile = __DIR__ . "/fixtures/file1.json";
+        $this->secondJsonFile = __DIR__ . "/fixtures/file2.json";
+        $this->firstYmlFile = __DIR__ . "/fixtures/file1.yml";
+        $this->secondYmlFile = __DIR__ . "/fixtures/file2.yml";
+    }
 
-  public function testMainGendiff(): void
-  {
-    $result = file_get_contents("tests/fixtures/stylishResult.txt");
-    $this->assertEquals($result, genDiff($this->firstJsonFile, $this->secondJsonFile));
-    $this->assertEquals($result, genDiff($this->firstYmlFile, $this->secondYmlFile));
-  }
+    public function testMainGendiff(): void
+    {
+        $result = file_get_contents("tests/fixtures/stylishResult.txt");
+        $this->assertEquals($result, genDiff($this->firstJsonFile, $this->secondJsonFile));
+        $this->assertEquals($result, genDiff($this->firstYmlFile, $this->secondYmlFile));
+    }
 
-  public function testPlainFormat(): void
-  {
-    $result = file_get_contents("tests/fixtures/plainResult.txt");
-    $this->assertEquals($result, genDiff($this->firstJsonFile, $this->secondJsonFile, "plain"));
-    $this->assertEquals($result, genDiff($this->firstYmlFile, $this->secondYmlFile, "plain"));
-  }
+    public function testPlainFormat(): void
+    {
+        $result = file_get_contents("tests/fixtures/plainResult.txt");
+        $this->assertEquals($result, genDiff($this->firstJsonFile, $this->secondJsonFile, "plain"));
+        $this->assertEquals($result, genDiff($this->firstYmlFile, $this->secondYmlFile, "plain"));
+    }
 
-  public function testJsonFormat(): void
-  {
-    $result = file_get_contents("tests/fixtures/jsonResult.txt");
-    $this->assertEquals($result, genDiff($this->firstJsonFile, $this->secondJsonFile, "json"));
-    $this->assertEquals($result, genDiff($this->firstYmlFile, $this->secondYmlFile, "json"));
-  }
+    public function testJsonFormat(): void
+    {
+        $result = file_get_contents("tests/fixtures/jsonResult.txt");
+        $this->assertEquals($result, genDiff($this->firstJsonFile, $this->secondJsonFile, "json"));
+        $this->assertEquals($result, genDiff($this->firstYmlFile, $this->secondYmlFile, "json"));
+    }
 }
