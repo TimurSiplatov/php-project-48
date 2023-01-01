b72b657ecc40c759ee7c01a5823b6cd097e9ef9f
TimurSiplatov <jjj_jjj_18@mail.ru>
2023-01-01 16:24:34 +0300

    fix errors

--- src/Parsers.php
+++ src/Parsers.php
@@ -7,6 +7,9 @@ use Symfony\Component\Yaml\Yaml;
 function parseFile(string $pathToFile)
 {
     $fileFormat = pathinfo($pathToFile, PATHINFO_EXTENSION);
+    if (file_get_contents($pathToFile) === false) {
+        throw new \Exception("Error, there is no file");
+    }
     if ($fileFormat === "json") {
         return json_decode(file_get_contents($pathToFile), true);
     } elseif ($fileFormat === "yaml" || $fileFormat === "yml") {
